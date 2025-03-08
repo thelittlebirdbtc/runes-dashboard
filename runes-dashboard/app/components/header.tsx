4ed9eca10eebf768e725853a2cc15e6dd71960b8
thelittlebirdbtc <thelittlebirdbtc@outlook.com>
2025-03-08 18:18:58 -0300

    initialize Next.js project with Tailwind CSS and ESLint configuration

---
+++ runes-dashboard/app/components/header.tsx
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+        <header className="flex items-center justify-between px-10 pt-10">
+            <div className="flex items-center justify-between gap-x-8">
+
+            </div>
+        </header>
+    )
+}
+
+export default Header;
