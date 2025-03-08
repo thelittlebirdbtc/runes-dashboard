104cfe32f67604b5a5bd967407cc6c6299315732
thelittlebirdbtc <thelittlebirdbtc@outlook.com>
2025-03-08 18:37:09 -0300

    add utility functions and button component; configure Tailwind CSS and update global styles

--- runes-dashboard/app/components/header.tsx
+++ runes-dashboard/app/components/header.tsx
@@ -1,9 +1,42 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import { buttonVariants } from '@/components/ui/button';
+
 function Header() {
     return (
         <header className="flex items-center justify-between px-10 pt-10">
             <div className="flex items-center justify-between gap-x-8">
+                
+                <Link href="/">
+                    <Image src="/runeslogo.png" alt="Runes Dashboard" width={40} height={40} />
+                </Link>
 
+                <Link
+                href={"https://leather.io/learn/bitcoin-runes"}
+                target="_blank"
+                className={buttonVariants({ variant: "link" })}
+                >
+                What are Runes?
+                </Link>
+
+                <Link
+                href={"https://www.hiro.so/blog/introducing-the-runes-api"}
+                target="_blank"
+                className={buttonVariants({ variant: "link" })}
+                >
+                What is the Runes API?
+                </Link>
+
+                <Link
+                href={"https://github.com/hirosystems/runehook"}
+                target="_blank"
+                className={buttonVariants({ variant: "link" })}
+                >
+                Runehook
+                </Link>
+                
             </div>
+            
         </header>
     )
 }
